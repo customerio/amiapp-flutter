d093a417acb9f03e806f89553c8c79140c34e45b
Shahroz Khan <shrozali@gmail.com>
2023-02-17 04:53:19 +0500

    added github actions to run deployment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:amiapp_flutter/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:amiapp_flutter/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
