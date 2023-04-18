8c3819a59117aa41f9c9de5da9f7a21fbf6b335a
Rehan <mrehan.official@gmail.com>
2023-04-18 17:02:28 +0500

    fix: support appium tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_driver/driver_extension.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import 'src/app.dart';
 
 void main() async {
+  // Flutter driver for UI automation in debug builds
+  if (kDebugMode) {
+    try {
+      enableFlutterDriverExtension();
+    } catch (exception) {
+      print(exception);
+    }
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
