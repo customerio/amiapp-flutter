975b0a7f6224322e62d196b678cfccda6b528185
Rehan <mrehan.official@gmail.com>
2023-07-12 11:54:49 +0500

    fix: tracking url in settings

--- lib/src/customer_io.dart
+++ lib/src/customer_io.dart
@@ -59,6 +59,7 @@ class CustomerIOSDK extends ChangeNotifier {
           apiKey: _sdkConfig?.apiKey ?? '',
           enableInApp: true,
           region: Region.us,
+          trackingApiUrl: _sdkConfig?.trackingUrl ?? '',
           //config options go here
           autoTrackDeviceAttributes:
               _sdkConfig?.deviceAttributesTrackingEnabled ?? true,
